f52bad28e9a561a0eef26d81b05f1866ae3b1498
mantasha <mantasah@gmail.com>
2026-04-30 09:33:55 +0530

    addind the data

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://13.204.45.167:8080";
+const BASE_URL = "http://localhost:8080";
 
 export const registerUser = (user) =>  axios.post(`${BASE_URL}/user/register`,user);
 
