e0e84a31f9b8ba4b6e4589ea5cc995cfd6f62d81
mantasha <mantasha@gmail.com>
2026-06-06 13:43:11 +0530

    adding the devops

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "/api";
 
 export const registerUser = (user) =>  axios.post(`${BASE_URL}/user/register`,user);
 
